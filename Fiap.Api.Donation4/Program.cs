88912dc5c8f803fb957cb05e4dc1dd2373da6eb2
Carlosedu11 <miguelodoom22@hotmail.com>
2025-04-10 08:48:56 -0300

    Finalizando Autenticação

--- Fiap.Api.Donation4/Program.cs
+++ Fiap.Api.Donation4/Program.cs
@@ -1,7 +1,11 @@
+using Fiap.Api.Donation4;
 using Fiap.Api.Donation4.Data;
 using Fiap.Api.Donation4.Repository;
 using Fiap.Api.Donation4.Repository.Interfaces;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,9 +23,45 @@ builder.Services.AddDbContext<DataContext>(
     options => options.UseSqlServer(connectionString).EnableSensitiveDataLogging() 
     );
 
-#region
+#region Repository
 builder.Services.AddScoped<ICategoriaRepository, CategoriaRepository>();
 builder.Services.AddScoped<IUsuarioRepository, UsuarioRepository>();
+#endregion
+
+#region Autenticacao
+var secretToken = Encoding.UTF8.GetBytes(Settings.SECRET_TOKEN);
+
+bool CustomLifetimeValidator(DateTime? notBefore, DateTime? expires, SecurityToken tokenToValidate, TokenValidationParameters @param)
+{
+    if (expires != null)
+    {
+        return expires > DateTime.UtcNow;
+    }
+    return false;
+}
+
+builder.Services
+     .AddAuthentication(a =>
+     {
+         a.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+         a.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+     })
+     .AddJwtBearer(opt =>
+     {
+         opt.RequireHttpsMetadata = false;
+         opt.SaveToken = true;
+         opt.TokenValidationParameters = new TokenValidationParameters
+         {
+             ValidateAudience = false,
+             ValidateIssuer = true,
+             ValidIssuer = "fiap",
+             IssuerSigningKey = new SymmetricSecurityKey(secretToken),
+             RequireExpirationTime = true,
+             LifetimeValidator = CustomLifetimeValidator
+         };
+     });
+
+
 #endregion
 
 
@@ -39,6 +79,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
